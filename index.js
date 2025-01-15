758ac337db9dd727f524abd8208d00779c6996f1
fullabeansbaby <linkeyart@gmail.com>
2025-01-15 15:53:35 -0500

    Updated index.js
    Pre-pity rate implementation

--- index.js
+++ index.js
@@ -20,20 +20,36 @@ client.on('messageCreate', message => {
 
     const userTriggers = {
         '130817946032275456': ['kelly'],
-        '218955017900654603': ['jigme', 'jstest']
+        '218955017900654603': ['jigmo'],
+        '427160105625059339': ['jigme'],
+        '512443803559264256': ['jigme']
     };
+
+    const universalTriggers = ['jigmo', 'gerbear', 'oh bother', 'jumpscare'];
 
     const lowerCaseContent = message.content.toLowerCase();
     const userTriggerWords = userTriggers[message.author.id];
 
-    if (userTriggerWords && userTriggerWords.some(trigger => lowerCaseContent.includes(trigger))) {
+    if (userTriggerWords && userTriggerWords.some(trigger => lowerCaseContent.includes(trigger)) ||
+        universalTriggers.some(trigger => lowerCaseContent.includes(trigger))) {
         console.log('Trigger detected');
-        message.reply('https://i.imgur.com/d7V4jkz.gif').then(sentMessage => {
+
+        let responseLink;
+        if (message.author.id === '218955017900654603' && lowerCaseContent.includes('jigmo')) {
+            responseLink = 'wink! https://i.imgur.com/IjuhJB7.gif';
+        } else {
+            const randomChance = Math.random();
+            responseLink = randomChance < 0.1
+                ? 'wink! https://i.imgur.com/IjuhJB7.gif'
+                : 'https://i.imgur.com/d7V4jkz.gif';
+        }
+
+        message.reply(responseLink).then(sentMessage => {
             setTimeout(() => {
                 sentMessage.delete().catch(error => {
                     console.error('Failed to delete the message:', error);
                 });
-            }, 2000); // Delete the message after 2 seconds
+            }, 2000);
         });
     }
 });
